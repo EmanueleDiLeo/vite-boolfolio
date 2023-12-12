902c56a2892925e65091c833cd362142ba5cec81
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-12-12 20:10:24 +0100

    [ADD]: type and technology text

--- src/components/partials/ProjectCard.vue
+++ src/components/partials/ProjectCard.vue
@@ -14,6 +14,11 @@ export default{
         <div class="card-body">
           <h3 class="card-title">{{ project.name }}</h3>
           <p class="card-text"><strong>Versione:</strong> {{project.version }} - <strong>Data:</strong> {{ project.date_updated }}</p>
+          <p class="card-text"><strong>Tipo:</strong> {{project.type?.name ?? 'NESSUNO'}}</p>
+          <div v-if="project.technologies.length !== 0">
+            <strong>Tecnologie:</strong>
+            <span v-for="technology in project.technologies" :key="technology.id" class="badge text-bg-success m-1">{{ technology.name }}</span>
+          </div>
           <router-link :to="{name: 'showProject', params:{slug: project.slug}}" class="btn btn-info">Info</router-link>
         </div>
       </div>
