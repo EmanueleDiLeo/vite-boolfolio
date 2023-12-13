bb6ff43765033128d444a61caec154aa8a851f00
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-12-13 17:52:38 +0100

    [ADD]: form contact

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name:'ContactForm',
+  methods:{
+    sendForm(){
+      console.log('OK')
+    },
+  },
+}
+</script>
+
+<template>
+
+  <div class="">
+    <form @submit.prevent="sendForm()"> 
+
+      <div class="mb-3">
+        <label for="name" class="form-label">Nome</label>
+        <input class="form-control" type="text">
+      </div>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input class="form-control" type="text">
+      </div>
+
+      <div class="mb-3">
+        <label for="message" class="form-label">Messaggio</label>
+        <textarea class="form-control" name="message" id="message" cols="30" rows="10"></textarea>
+      </div>
+
+      <div>
+        <button type="submit" class="btn btn-primary">Invia</button>
+      </div>
+
+    </form>
+  </div>
+  
+</template>
+
+<style>
+
+</style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,17 @@
 <script>
+import ContactForm from '../components/ContactForm.vue';
 export default {
   name:'Contact',
+  components:{
+    ContactForm,
+  },
 }
 </script>
 
 <template>
 <div class="container">
   <h1 class="text-center">Contatti</h1>
+  <ContactForm />
 </div>
 </template>
 
